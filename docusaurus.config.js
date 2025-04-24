6a4e5d84f60816679458a4bae563f31687346877
Santiago Armesto <contactosantiagoarmesto@gmail.com>
2025-04-24 18:19:14 -0300

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'armestosantiago', // Usually your GitHub org/user name.
-  projectName: 'example-githubpage', // Usually your repo name.
+  projectName: 'example-githupage', // Usually your repo name.
   deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
